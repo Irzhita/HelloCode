c57d564c6f023194ad46411ca07f47ffe4787e71
Irzhita <piccadilly@bk.ru>
2022-09-29 13:51:21 +0300

    Example 002 Hello User project

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿// Hello, User
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
+Console.Write("Введите ваше имя ");
+username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
