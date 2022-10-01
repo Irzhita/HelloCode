eef0f3655d1684e0c0df27d8f66d9d67da1c53e7
Irzhita <piccadilly@bk.ru>
2022-10-01 09:07:03 +0300

    Семинар 1. Домашняя работа. Задача 4. Определить максимум из трех чисел

---
+++ SEM1_HomeW_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > b)
+{
+    if (a > c)
+        Console.WriteLine("max = " + a);
+    else
+        Console.WriteLine("max = " + c);
+}
+else
+{
+    if (b > c)
+        Console.WriteLine("max = " + b);
+    else
+        Console.WriteLine("max = " + c);
+}
