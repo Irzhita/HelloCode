3f4091da51fbcad421b98035c32103937db1713d
Irzhita <piccadilly@bk.ru>
2022-10-03 11:23:12 +0300

    Example 011 MethodArray. Looking for an array element with given value

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 4};
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (index == find)        
+    {{
+        Console.WriteLine(index);
+        break; //ищется только первый элемент, равный find
+    }}
+index++;
+}
