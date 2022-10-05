55fb2878a140de10cd2834d1975bcb2d20f0c057
Irzhita <piccadilly@bk.ru>
2022-10-05 16:06:24 +0300

    Семинар 2. Домашняя работа. Задача 15. Выходной день или нет?

---
+++ SEM2_HomeW_15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным
+
+Console.Clear();
+
+Console.Write("Введите цифру дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись! Введите снова порядковый номер дня недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n == 6 || n == 7)
+    Console.WriteLine("ура, выходной");
+else
+    Console.WriteLine("на работу!");
