0c3ee01bf2deed48d8a1b6b1b555ff37af05d87e
Irzhita <piccadilly@bk.ru>
2022-10-07 17:19:13 +0300

    Семинар 3. Домашняя работа. Дополнительно. Сбор черники

---
+++ SEM3_HomeW_Plus/Program.cs
@@ -0,0 +1,45 @@
+﻿// Сбор черники https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=5&id_topic=113&id_problem=695
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+Console.Clear();
+int n = new Random().Next(3,11);
+Console.Write($"Количество кустов черники: {n} с урожайностью ");
+// Тестим пока для 10 кустов
+
+int[] array = new int[n]; 
+
+FillArray(array);
+PrintArray(array);
+
+int max = 0;
+
+for (int i = 2; i <= n-1; i++)
+{
+    int sum3 = array[i-2] + array[i-1] + array[i];
+    if (max < sum3) max = sum3;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Максимальный сбор - {max}");
