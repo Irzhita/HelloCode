f14ad3d54cf47bba50c164d7dd2e36c6c6b3ae94
Irzhita <piccadilly@bk.ru>
2022-10-12 17:56:45 +0300

    Семинар 4. Домашняя работа. Задача 25. Возведение в степень числа

---
+++ SEM4_HomeW_25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double result = 1;
+
+for (int i = 1; i <= b; i++)
+    result = result*a;
+    
+Console.WriteLine($"a ** b = {result}");
