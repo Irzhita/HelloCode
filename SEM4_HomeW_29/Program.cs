c5a120207d08f3854170b05f9868a1e492aafdba
Irzhita <piccadilly@bk.ru>
2022-10-12 17:59:34 +0300

    Семинар 4. Домашняя работа. Задача 29. Ввод массива пользователем и вывод его на экран

---
+++ SEM4_HomeW_29/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов (вводит пользователь) и выводит их на экран.
+
+Console.Clear();
+int[] array = new int[8]; 
+Console.WriteLine("Введите элементы массива: ");
+for (int i = 0; i < array.Length; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
