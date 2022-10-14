04b6ac2dd8afd611884f5fd8ec4a5b7cec44f85e
Irzhita <piccadilly@bk.ru>
2022-10-14 20:39:38 +0300

    Семинар 5. Домашняя работа. Задача 36. Сумма элементов на нечетных позициях в массиве

---
+++ SEM5_HomeW_36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11);
+}
+
+void SumOddPositions(int[] array)
+{
+    int sum = 0; 
+    for (int i = 1; i < array.Length; i+=2)
+        sum = sum + array[i];
+    Console.WriteLine($"Сумма элементов на нечетных позициях в массиве равна {sum}");
+}
+
+Console.Clear();
+
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+FillArray(array);
+Console.WriteLine($"[{string.Join(",", array)}]");
+
+SumOddPositions(array);
