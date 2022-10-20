24b0cfcc24d7f32217c7b3d100f6ebdfde5f2407
Irzhita <piccadilly@bk.ru>
2022-10-20 11:30:21 +0300

    Семинар 6. Домашняя работа. Задача 41. Подсчет чисел больше нуля

---
+++ SEM6_HomeW_41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+int count = 0;
+Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите последовательность чисел: ");
+
+for (int i = 1; i <= m; i++)
+
+    if (Convert.ToInt32(Console.ReadLine()) > 0) count = count +1;
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
