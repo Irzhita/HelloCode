1050bc17ee68f7a5dae249155676854b4fb92c6e
Irzhita <piccadilly@bk.ru>
2022-10-21 20:58:56 +0300

    Семинар 7. Домашняя работа. Задача 52. Среднее арифметическое по столбцам для двумерного массива

---
+++ SEM7_HomeW_52/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           matrix[i,j] = new Random().Next(1, 21);
+           Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();    
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите количество столбцов массива: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+
+int[,] mymatrix = new int[n, m];
+FillMatrix(mymatrix);
+
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое по столбцам: ");
+
+double[] arithmean = new double[m];
+
+
+for (int j = 0; j < mymatrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < mymatrix.GetLength(0); i++)
+    {
+        sum = sum + mymatrix[i, j];
+    }
+arithmean[j] = Math.Round(sum/n,2);
+Console.Write($"{string.Join(" ", arithmean[j])} \t");
+}
+
+Console.WriteLine();
