1cd5657f2f06c9bf12a4a58c96ca74e4a28ec629
Irzhita <piccadilly@bk.ru>
2022-11-09 10:03:49 +0300

    Семинар 8. Домашняя работа. Задача 56. Найти строку с наименьшей суммой элементов в двумерном массиве

---
+++ SEM8_HomeW_56/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку 
+// с наименьшей суммой элементов.
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 21);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FindPlaceOfMinimum(int[] array)
+{
+    int min = array[0];
+    int num = 0;
+    for (int i = 1; i < array.Length; i++)
+        if (min > array[i]) 
+        {
+            min = array[i];
+            num = i;
+        }
+    Console.WriteLine($"Строка с наименьшей суммой элементов - {num+1} ");
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк и столбцов: ");
+string[] num = Console.ReadLine().Split(' ');
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+int[,] matrix = new int[n, m];
+FillMatrix(matrix); 
+Console.WriteLine();
+
+int[] sums = new int[matrix.GetLength(0)]; 
+Console.Write($"Суммы строк: ");
+{
+    for (int i = 0; i < matrix.GetLength(0); i++ )
+    {
+        int sum = 0;
+        {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            sum = sum + matrix[i, j];
+            sums[i] = sum;
+            Console.Write($"{sums[i]} ");
+        }
+    }
+}
+Console.WriteLine();
+FindPlaceOfMinimum(sums);
