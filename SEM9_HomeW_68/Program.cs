5678427de46d11b014ee9c7fb19149901898ce80
Irzhita <piccadilly@bk.ru>
2022-11-12 14:57:55 +0300

    Семинар 9. Домашняя работа. Задача 68. Функция Аккермана

---
+++ SEM9_HomeW_68/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Timers;
+// Задача 68. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа n и m.
+
+int AccermanFunction(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+        if (n == 0)
+            return AccermanFunction(m - 1, 1);
+        else
+            return AccermanFunction(m - 1, AccermanFunction(m, n-1));
+}
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+while (m < 0)
+{
+    Console.Write("Число должно быть неотрицательным: ");
+    m = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 0)
+{
+    Console.Write("Введите неотрицательное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+
+Console.WriteLine(AccermanFunction(m, n));
