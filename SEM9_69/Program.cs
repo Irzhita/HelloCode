01c63789020c2c436bea67fabf0a55588b1e7435
Irzhita <piccadilly@bk.ru>
2022-11-12 10:40:55 +0300

    Семинар 9. Задача 69. Возведение числа в степень с помощью рекурсии

---
+++ SEM9_69/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69. Напишите программу, которая на вход принимает два числа А и В, и
+// возводит число А в целую степень В с помощью рекурсии.
+
+int DegreeOfNumber(int a, int b)
+{
+    if (b == 0)
+        return 1; // число в 0-ой степени равно 1
+    return DegreeOfNumber(a, b-1) * a;
+}
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(DegreeOfNumber(a, b));
