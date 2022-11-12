acec8d989f6c7bf952e42a697f20aca47b45beae
Irzhita <piccadilly@bk.ru>
2022-11-12 10:00:02 +0300

    Семинар 9. Задача 65. Вывод чисел от M до N с помощью рекурсии

---
+++ SEM9_65/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 65. Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N.
+
+string ResultNumbers(int m, int n)
+{
+    if (n == m)
+        return $"{m}";
+    return ResultNumbers(m, n-1) + ", " + Convert.ToString(n);
+
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ResultNumbers(m, n));
