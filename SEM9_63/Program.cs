7f330499c4e0a60fee729e215d89020a296347a3
Irzhita <piccadilly@bk.ru>
2022-11-12 09:42:15 +0300

    Семинар 9. Задача 63. Вывод чисел от 1 до N с помощью рекурсии

---
+++ SEM9_63/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Задача 63. Напишите программу, которая выведет все натуральные числа
+// в промежутке от 1 до N. (через рекурсию)
+
+void Numbers(int n, int j)
+{
+    if (n == j)
+        Console.Write($" {n} ");
+    else
+    {
+        Console.Write($" {j} "); 
+        Numbers(n, ++j); 
+    }
+}
+
+
+string ResultNumbers(int n)
+{
+    if (n == 1)
+        return "1";
+    return ResultNumbers(n-1) + ", " + Convert.ToString(n);
+
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), j = 1;
+
+Console.WriteLine(ResultNumbers(n));
+Numbers(n, j);
