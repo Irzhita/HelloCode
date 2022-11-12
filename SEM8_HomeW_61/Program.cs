dd64fc85d5ef2408675404fa17d17609ec175cc9
Irzhita <piccadilly@bk.ru>
2022-11-12 07:41:44 +0300

    Семинар 8. Домашняя работа. Задача 61. Треугольник Пифагора

---
+++ SEM8_HomeW_61/Program.cs
@@ -0,0 +1,31 @@
+﻿// Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+
+int factorial(int n)
+{
+    int x = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+void PythagorasTriangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= (n - i); j++) // делаем отступы справа
+            Console.Write("   "); 
+        for (int j = 0; j <= i; j++)
+            Console.Write($"    {factorial(i) / (factorial(j) * factorial(i - j))}"); //формула вычисления элементов треугольника
+        Console.WriteLine();
+        Console.WriteLine(); 
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк треугольника Пифагора: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PythagorasTriangle(n);
