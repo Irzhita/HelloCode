ef9c3a77f093660ef3a90c87bc1df95675b92954
Irzhita <piccadilly@bk.ru>
2022-11-12 10:29:32 +0300

    Семинар 9. Задача 67. Подсчет суммы цифр числа с помощью рекурсии

---
+++ SEM9_67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67. Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+int NumbersSum(int n)
+{
+    if (n / 10 == 0)
+        return n;
+    return NumbersSum(n / 10) + n % 10;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumbersSum(n));
