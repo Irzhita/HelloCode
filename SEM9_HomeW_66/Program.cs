6a8ca495528e2809e5a378f9aafd97a7dbfd176e
Irzhita <piccadilly@bk.ru>
2022-11-12 10:52:39 +0300

    Семинар 9. Домашняя работа. Задача 66. Подсчет суммы чисел между M и N с помощью рекурсии

---
+++ SEM9_HomeW_66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66. Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов 
+// в промежутке от M до N (с помощью рекурсии).
+
+int SumOfNumbers(int m, int n)
+{
+    if (n == m)
+        return n;
+    return SumOfNumbers(m, n-1) + n;
+
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumOfNumbers(m, n));
